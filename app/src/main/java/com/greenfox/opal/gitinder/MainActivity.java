4843b3e1b264f375d017198c9ac357efa8966c11
Nagy Dora <dorinagy06@gmail.com>
2017-06-14 16:47:05 +0200

    show app icon in action bar

--- app/src/main/java/com/greenfox/opal/gitinder/MainActivity.java
+++ app/src/main/java/com/greenfox/opal/gitinder/MainActivity.java
@@ -1,6 +1,7 @@
 package com.greenfox.opal.gitinder;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -11,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setLogo(R.mipmap.gitinder_logo);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
         setContentView(R.layout.activity_main);
     }
 
