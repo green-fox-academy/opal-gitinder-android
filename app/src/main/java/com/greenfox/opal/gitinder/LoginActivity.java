a30d2d04acf904eff1a0b81936b3877bd6f43c82
balintvecsey <balint.vecsey@gmail.com>
2017-06-14 17:07:22 +0200

    add login dialog as an AlertDialog but it needs to push a button to popup

--- app/src/main/java/com/greenfox/opal/gitinder/LoginActivity.java
+++ app/src/main/java/com/greenfox/opal/gitinder/LoginActivity.java
@@ -1,13 +1,47 @@
 package com.greenfox.opal.gitinder;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+  Button loginButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
+    onButtonClickListener();
+  }
+
+  public void onButtonClickListener() {
+    loginButton = (Button) findViewById(R.id.login_button);
+    loginButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(LoginActivity.this);
+        a_builder.setMessage(R.string.dialog_message)
+            .setCancelable(false)
+            .setPositiveButton(R.string.dialog_button_login,new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+              }
+            })
+            .setNegativeButton(R.string.dialog_button_exit,new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                finish();
+              }
+            }) ;
+        AlertDialog alert = a_builder.create();
+        alert.setTitle(R.string.dialog_title);
+        alert.show();
+      }
+    });
   }
 }
