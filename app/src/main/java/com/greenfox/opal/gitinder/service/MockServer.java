61a0db20d097ccd12218e6596a6bb124a54ba13b
Milan Kovacs <kovacsmilan85@gmail.com>
2017-06-14 12:36:45 +0200

    create and use MockCall class

--- app/src/main/java/com/greenfox/opal/gitinder/service/MockServer.java
+++ app/src/main/java/com/greenfox/opal/gitinder/service/MockServer.java
@@ -4,25 +4,16 @@ import com.greenfox.opal.gitinder.ApiService;
 import com.greenfox.opal.gitinder.model.LoginRequest;
 import com.greenfox.opal.gitinder.model.StatusResponse;
 
-import java.io.IOException;
-
-import okhttp3.Request;
-import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.http.Body;
 
 public class MockServer implements ApiService {
     @Override
-    public Call<StatusResponse> login(@Body final LoginRequest loginRequest) {
-        return new Call<StatusResponse>() {
+    public MockCall login(@Body final LoginRequest loginRequest) {
+        return new MockCall() {
             @Override
-            public Response<StatusResponse> execute() throws IOException {
-                return null;
-            }
-
-            @Override
-            public void enqueue(Callback<StatusResponse> callback) {
+            public void enqueue(Callback callback) {
                 StatusResponse response;
                 if (loginRequest.getUsername().isEmpty() || loginRequest.getAccessToken().isEmpty()) {
                     String message = "Missing parameter(s):";
@@ -38,31 +29,6 @@ public class MockServer implements ApiService {
                     response = new StatusResponse("ok", "abc123");
                 }
                 callback.onResponse(null, Response.success(response));
-            }
-
-            @Override
-            public boolean isExecuted() {
-                return false;
-            }
-
-            @Override
-            public void cancel() {
-
-            }
-
-            @Override
-            public boolean isCanceled() {
-                return false;
-            }
-
-            @Override
-            public Call<StatusResponse> clone() {
-                return null;
-            }
-
-            @Override
-            public Request request() {
-                return null;
             }
         };
     }
