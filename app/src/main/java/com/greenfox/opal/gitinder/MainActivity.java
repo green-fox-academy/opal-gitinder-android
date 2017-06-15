30478ba8818a4ec9773fc6a6f631c83b5e1862fe
Nagy Dora <dorinagy06@gmail.com>
2017-06-15 13:42:10 +0200

    set tab names

--- app/src/main/java/com/greenfox/opal/gitinder/MainActivity.java
+++ app/src/main/java/com/greenfox/opal/gitinder/MainActivity.java
@@ -3,9 +3,7 @@ package com.greenfox.opal.gitinder;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +14,28 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setLogo(R.mipmap.gitinder_logo);
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
-        setContentView(R.layout.activity_main);
-    }
 
-    public void sendMessage(View view) {
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(message);
+        setContentView(R.layout.activity_main);
+
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("SWIPING");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("SWIPING");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("MATCHES");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("MATCHES");
+        host.addTab(spec);
+
+        //Tab 3
+        spec = host.newTabSpec("SETTINGS");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("SETTINGS");
+        host.addTab(spec);
     }
 }
