da52d1f05598fb7b17b39459a8a988044a9aa213
Milan Kovacs <kovacsmilan85@gmail.com>
2017-06-16 09:38:47 +0200

    create Snippet model

---
+++ app/src/main/java/com/greenfox/opal/gitinder/model/Snippet.java
@@ -0,0 +1,17 @@
+package com.greenfox.opal.gitinder.model;
+
+public class Snippet {
+    String language;
+    String code;
+
+    public Snippet() {
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
