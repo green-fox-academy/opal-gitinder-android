2471fd840b009726e975dd855e02ff3ba3e62f12
balintvecsey <balint.vecsey@gmail.com>
2017-06-16 11:09:57 +0200

    move authentication from MainActivity to LoginActivity

--- app/src/main/java/com/greenfox/opal/gitinder/LoginActivity.java
+++ app/src/main/java/com/greenfox/opal/gitinder/LoginActivity.java
@@ -5,6 +5,22 @@ import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.wuman.android.auth.AuthorizationDialogController;
+import com.wuman.android.auth.AuthorizationFlow;
+import com.wuman.android.auth.DialogFragmentController;
+import com.wuman.android.auth.OAuthManager;
+import com.wuman.android.auth.OAuthManager.OAuthCallback;
+import com.wuman.android.auth.OAuthManager.OAuthFuture;
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,7 +44,7 @@ public class LoginActivity extends AppCompatActivity {
         .setPositiveButton(R.string.dialog_button_login, new DialogInterface.OnClickListener() {
           @Override
           public void onClick(DialogInterface dialog, int which) {
-            recreate(); //change after add oauth login
+            authentication();
           }
         })
         .setNegativeButton(R.string.dialog_button_exit, new DialogInterface.OnClickListener() {
@@ -40,5 +56,66 @@ public class LoginActivity extends AppCompatActivity {
     AlertDialog alert = a_builder.create();
     alert.setTitle(R.string.dialog_title);
     alert.show();
+  }
+
+  public void authentication() {
+    OAuthManager oAuthManager = new OAuthManager(buildAuthorizationFlow(), createGitHubLoginDialog());
+    oAuthManager.authorizeExplicitly("userID", new OAuthCallback<Credential>() {
+      @Override
+      public void run(OAuthFuture<Credential> future) {
+        try {
+          Log.d("success", future.getResult().getAccessToken());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }, null);
+  }
+
+  public AuthorizationDialogController createGitHubLoginDialog() {
+    AuthorizationDialogController controller =
+        new DialogFragmentController(getFragmentManager()) {
+          @Override
+          public String getRedirectUri() throws IOException {
+            return "http://gitinder.herokuapp.com/callback";
+          }
+
+          @Override
+          public boolean isJavascriptEnabledForWebView() {
+            return true;
+          }
+
+          @Override
+          public boolean disableWebViewCache() {
+            return false;
+          }
+
+          @Override
+          public boolean removePreviousCookie() {
+            return false;
+          }
+        };
+
+    return controller;
+  }
+
+  public AuthorizationFlow buildAuthorizationFlow() {
+    AuthorizationFlow.Builder builder = new AuthorizationFlow.Builder(
+        BearerToken.authorizationHeaderAccessMethod(),
+        AndroidHttp.newCompatibleTransport(),
+        new JacksonFactory(),
+        new GenericUrl("https://github.com/login/oauth/access_token"),
+        new ClientParametersAuthentication(getResources().getString(R.string.CLIENT_ID), getResources().getString(R.string.CLIENT_SECRET)),
+        getResources().getString(R.string.CLIENT_ID),
+        "http://github.com/login/oauth/authorize");
+    builder.setRequestInitializer(new HttpRequestInitializer() {
+      @Override
+      public void initialize(HttpRequest request) throws IOException {
+        request.getHeaders().setAccept("application/json");
+      }
+    });
+
+    AuthorizationFlow flow = builder.build();
+    return flow;
   }
 }
--- app/src/main/java/com/greenfox/opal/gitinder/MainActivity.java
+++ app/src/main/java/com/greenfox/opal/gitinder/MainActivity.java
@@ -13,27 +13,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.api.client.auth.oauth2.BearerToken;
-import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.greenfox.opal.gitinder.model.LoginRequest;
 import com.greenfox.opal.gitinder.response.LoginResponse;
 import com.greenfox.opal.gitinder.service.MockServer;
-
-import com.wuman.android.auth.AuthorizationDialogController;
-import com.wuman.android.auth.AuthorizationFlow;
-import com.wuman.android.auth.DialogFragmentController;
-import com.wuman.android.auth.OAuthManager;
-
-import com.wuman.android.auth.OAuthManager.OAuthCallback;
-import com.wuman.android.auth.OAuthManager.OAuthFuture;
-import java.io.IOException;
-
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -64,58 +46,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         onLogin("Bond", "abcd1234");
-
-        AuthorizationFlow.Builder builder = new AuthorizationFlow.Builder(
-                BearerToken.authorizationHeaderAccessMethod(),
-                AndroidHttp.newCompatibleTransport(),
-                new JacksonFactory(),
-                new GenericUrl("https://github.com/login/oauth/access_token"),
-                new ClientParametersAuthentication(getResources().getString(R.string.CLIENT_ID), getResources().getString(R.string.CLIENT_SECRET)),
-            getResources().getString(R.string.CLIENT_ID),
-                "http://github.com/login/oauth/authorize");
-            builder.setRequestInitializer(new HttpRequestInitializer() {
-                                      @Override
-                                      public void initialize(HttpRequest request) throws IOException {
-                                        request.getHeaders().setAccept("application/json");
-                                      }
-                                    });
-        AuthorizationFlow flow = builder.build();
-
-        AuthorizationDialogController controller =
-                new DialogFragmentController(getFragmentManager()) {
-                    @Override
-                    public String getRedirectUri() throws IOException {
-                        return "http://gitinder.herokuapp.com/callback";
-                    }
-
-                    @Override
-                    public boolean isJavascriptEnabledForWebView() {
-                        return true;
-                    }
-
-                    @Override
-                    public boolean disableWebViewCache() {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean removePreviousCookie() {
-                        return false;
-                    }
-                };
-
-        OAuthManager oAuthManager = new OAuthManager(flow, controller);
-        oAuthManager.authorizeExplicitly("userID", new OAuthCallback<Credential>() {
-            @Override
-            public void run(OAuthFuture<Credential> future) {
-            try {
-              Log.d("success", future.getResult().getAccessToken());
-            } catch (IOException e) {
-              e.printStackTrace();
-            }
-          }
-        }, null);
-
         checkLogin();
     }
 
