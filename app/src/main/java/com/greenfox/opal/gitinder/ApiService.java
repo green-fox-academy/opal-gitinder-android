0434896eafca3abf83e07a1f9c9b9e5039be197f
balintvecsey <balint.vecsey@gmail.com>
2017-06-19 15:42:15 +0200

    add /profile endpoint

--- app/src/main/java/com/greenfox/opal/gitinder/ApiService.java
+++ app/src/main/java/com/greenfox/opal/gitinder/ApiService.java
@@ -5,9 +5,14 @@ import com.greenfox.opal.gitinder.response.LoginResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiService {
     @POST("/login")
     Call<LoginResponse> login(@Body LoginRequest loginRequest);
+
+    @GET("/profile/{user}")
+    Call<List<Profile>> getProfileInfos(@Path("user") String userID);
 }
