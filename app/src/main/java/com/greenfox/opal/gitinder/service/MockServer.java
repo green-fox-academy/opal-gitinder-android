ee408cf9e235f127aeb46adefb2ec8f418195d12
Nagy Dora <dorinagy06@gmail.com>
2017-06-19 17:04:56 +0200

    add swiping endpoint

--- app/src/main/java/com/greenfox/opal/gitinder/service/MockServer.java
+++ app/src/main/java/com/greenfox/opal/gitinder/service/MockServer.java
@@ -1,12 +1,18 @@
 package com.greenfox.opal.gitinder.service;
 
+import com.greenfox.opal.gitinder.model.Profile;
+
 import com.greenfox.opal.gitinder.ApiService;
+import com.greenfox.opal.gitinder.Direction;
 import com.greenfox.opal.gitinder.model.LoginRequest;
 import com.greenfox.opal.gitinder.response.LoginResponse;
 
+import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
 
 public class MockServer implements ApiService {
     @Override
@@ -31,5 +37,12 @@ public class MockServer implements ApiService {
                 callback.onResponse(null, Response.success(response));
             }
         };
+    }
+
+    @Override
+    public Call<Profile> swiping(@Header(value = "X-GiTinder-token") String token,
+                                 @Path("username") String username,
+                                 @Path("direction") Enum<Direction> direction) {
+        return null;
     }
 }
