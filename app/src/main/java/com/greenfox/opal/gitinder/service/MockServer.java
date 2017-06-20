79706336e43a9c72ee953bf55ce7a21f0000d505
balintvecsey <balint.vecsey@gmail.com>
2017-06-20 10:29:44 +0200

    fix indentation

--- app/src/main/java/com/greenfox/opal/gitinder/service/MockServer.java
+++ app/src/main/java/com/greenfox/opal/gitinder/service/MockServer.java
@@ -14,48 +14,49 @@ import retrofit2.http.Body;
 import retrofit2.http.Header;
 
 public class MockServer implements ApiService {
-    @Override
-    public MockCall<LoginResponse> login(@Body final LoginRequest loginRequest) {
-        return new MockCall<LoginResponse>() {
-            @Override
-            public void enqueue(Callback callback) {
-                LoginResponse response;
-                if (loginRequest.getUsername().isEmpty() || loginRequest.getAccessToken().isEmpty()) {
-                    String message = "Missing parameter(s):";
-                    if (loginRequest.getUsername().isEmpty()) {
-                        message += " username";
-                    }
-                    if (loginRequest.getAccessToken().isEmpty()) {
-                        message += " accessToken";
-                    }
-                    message += "!";
-                    response = new LoginResponse(message);
-                } else {
-                    response = new LoginResponse(loginRequest.getUsername(), loginRequest.getAccessToken());
-                }
-                callback.onResponse(null, Response.success(response));
-            }
-        };
-    }
 
-    @Override
-    public MockCall<Profile> getProfileInfos(@Header("X-GiTinder-token") final String token) {
-        return new MockCall<Profile>() {
-            @Override
-            public void enqueue(Callback callback) {
-                ProfileResponse profileResponse;
-                if (token.isEmpty()) {
-                    profileResponse = new ProfileResponse("Unauthorized request!");
-                } else {
-                    List<String> repos = new ArrayList<>();
-                    List<String> languages = new ArrayList<>() ;
-                    repos.add("opal-gitinder-android");
-                    languages.add("Java");
-                    Profile profile = new Profile("balintvecsey", "avatar.png", repos, languages);
-                    profileResponse = new ProfileResponse(profile);
-                }
-                callback.onResponse(null, Response.success(profileResponse));
-            }
-        };
-    }
+  @Override
+  public MockCall<LoginResponse> login(@Body final LoginRequest loginRequest) {
+    return new MockCall<LoginResponse>() {
+      @Override
+      public void enqueue(Callback callback) {
+        LoginResponse response;
+        if (loginRequest.getUsername().isEmpty() || loginRequest.getAccessToken().isEmpty()) {
+          String message = "Missing parameter(s):";
+          if (loginRequest.getUsername().isEmpty()) {
+            message += " username";
+          }
+          if (loginRequest.getAccessToken().isEmpty()) {
+            message += " accessToken";
+          }
+          message += "!";
+          response = new LoginResponse(message);
+        } else {
+          response = new LoginResponse(loginRequest.getUsername(), loginRequest.getAccessToken());
+        }
+        callback.onResponse(null, Response.success(response));
+      }
+    };
+  }
+
+  @Override
+  public MockCall<Profile> getProfileInfos(@Header("X-GiTinder-token") final String token) {
+    return new MockCall<Profile>() {
+      @Override
+      public void enqueue(Callback callback) {
+        ProfileResponse profileResponse;
+        if (token.isEmpty()) {
+          profileResponse = new ProfileResponse("Unauthorized request!");
+        } else {
+          List<String> repos = new ArrayList<>();
+          List<String> languages = new ArrayList<>();
+          repos.add("opal-gitinder-android");
+          languages.add("Java");
+          Profile profile = new Profile("balintvecsey", "avatar.png", repos, languages);
+          profileResponse = new ProfileResponse(profile);
+        }
+        callback.onResponse(null, Response.success(profileResponse));
+      }
+    };
+  }
 }
