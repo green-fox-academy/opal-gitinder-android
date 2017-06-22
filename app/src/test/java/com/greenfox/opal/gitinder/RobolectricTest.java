b58be804ee7276407719982970c265ef55ad75c3
balintvecsey <balint.vecsey@gmail.com>
2017-06-22 11:09:40 +0200

    solved Roboelectric issue

--- app/src/test/java/com/greenfox/opal/gitinder/RobolectricTest.java
+++ app/src/test/java/com/greenfox/opal/gitinder/RobolectricTest.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
