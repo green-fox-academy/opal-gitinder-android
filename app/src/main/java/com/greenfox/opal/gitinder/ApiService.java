9160d4a2dce178583092e766f9cb23b30aa1cb4c
Garlyle <kovacsmilan85@gmail.com>
2017-06-27 09:45:45 +0200

    create endpoint and mock implementation

--- app/src/main/java/com/greenfox/opal/gitinder/ApiService.java
+++ app/src/main/java/com/greenfox/opal/gitinder/ApiService.java
@@ -2,12 +2,19 @@ package com.greenfox.opal.gitinder;
 
 import com.greenfox.opal.gitinder.model.LoginRequest;
 import com.greenfox.opal.gitinder.model.response.LoginResponse;
+import com.greenfox.opal.gitinder.model.response.MatchesResponse;
+import com.greenfox.opal.gitinder.service.MockCall;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface ApiService {
     @POST("/login")
     Call<LoginResponse> login(@Body LoginRequest loginRequest);
+
+    @GET("/matches")
+    MockCall<MatchesResponse> getMatches(@Header("X-GiTinder-token") String token);
 }
--- app/src/main/java/com/greenfox/opal/gitinder/service/MockServer.java
+++ app/src/main/java/com/greenfox/opal/gitinder/service/MockServer.java
@@ -3,10 +3,12 @@ package com.greenfox.opal.gitinder.service;
 import com.greenfox.opal.gitinder.ApiService;
 import com.greenfox.opal.gitinder.model.LoginRequest;
 import com.greenfox.opal.gitinder.model.response.LoginResponse;
+import com.greenfox.opal.gitinder.model.response.MatchesResponse;
 
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
 
 public class MockServer implements ApiService {
     @Override
@@ -27,6 +29,22 @@ public class MockServer implements ApiService {
                     response = new LoginResponse(message);
                 } else {
                     response = new LoginResponse(loginRequest.getUsername(), loginRequest.getAccessToken());
+                }
+                callback.onResponse(null, Response.success(response));
+            }
+        };
+    }
+
+    @Override
+    public MockCall<MatchesResponse> getMatches(@Header("X-GiTinder-token") final String token) {
+        return new MockCall<MatchesResponse>() {
+            @Override
+            public void enqueue(Callback callback) {
+                MatchesResponse response;
+                if (token.isEmpty()) {
+                    response = new MatchesResponse("Unauthorized request!");
+                } else {
+                    response = new MatchesResponse();
                 }
                 callback.onResponse(null, Response.success(response));
             }
