265f57d8e8bcbf169e3ff880df0a1d53f1c78da7
Garlyle <kovacsmilan85@gmail.com>
2017-06-28 11:45:01 +0200

    add empty constructor for Match

--- app/src/main/java/com/greenfox/opal/gitinder/model/response/Match.java
+++ app/src/main/java/com/greenfox/opal/gitinder/model/response/Match.java
@@ -4,25 +4,28 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 
 public class Match {
-    String username;
-    long matched_at;
-    ArrayList<String> messages;
+  String username;
+  long matched_at;
+  ArrayList<String> messages;
 
-    public Match(String username, long time) {
-        this.username = username;
-        matched_at = time;
-        messages = new ArrayList<>();
-    }
+  public Match() {
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public Match(String username, long time) {
+    this.username = username;
+    matched_at = time;
+    messages = new ArrayList<>();
+  }
 
-    public long getMatched_at() {
-        return matched_at;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public ArrayList<String> getMessages() {
-        return messages;
-    }
+  public long getMatched_at() {
+    return matched_at;
+  }
+
+  public ArrayList<String> getMessages() {
+    return messages;
+  }
 }
