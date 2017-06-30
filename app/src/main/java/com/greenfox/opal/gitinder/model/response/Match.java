714bddafea5889915a234e3372cd3684c39f17fb
Garlyle <kovacsmilan85@gmail.com>
2017-06-30 11:02:14 +0200

    fix indentation

--- app/src/main/java/com/greenfox/opal/gitinder/model/response/Match.java
+++ app/src/main/java/com/greenfox/opal/gitinder/model/response/Match.java
@@ -3,25 +3,25 @@ package com.greenfox.opal.gitinder.model.response;
 import java.util.ArrayList;
 
 public class Match {
-    String username;
-    long matched_at;
-    ArrayList<String> messages;
+  String username;
+  long matched_at;
+  ArrayList<String> messages;
 
-    public Match(String username, long time) {
-        this.username = username;
-        matched_at = time;
-        messages = new ArrayList<>();
-    }
+  public Match(String username, long time) {
+    this.username = username;
+    matched_at = time;
+    messages = new ArrayList<>();
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public long getMatched_at() {
-        return matched_at;
-    }
+  public long getMatched_at() {
+    return matched_at;
+  }
 
-    public ArrayList<String> getMessages() {
-        return messages;
-    }
+  public ArrayList<String> getMessages() {
+    return messages;
+  }
 }
--- app/src/main/java/com/greenfox/opal/gitinder/model/response/MatchesResponse.java
+++ app/src/main/java/com/greenfox/opal/gitinder/model/response/MatchesResponse.java
@@ -3,20 +3,20 @@ package com.greenfox.opal.gitinder.model.response;
 import java.util.List;
 
 public class MatchesResponse extends BaseResponse {
-    List<Match> matches;
+  List<Match> matches;
 
-    public MatchesResponse() {
-    }
+  public MatchesResponse() {
+  }
 
-    public MatchesResponse(List<Match> list) {
-        matches = list;
-    }
+  public MatchesResponse(List<Match> list) {
+    matches = list;
+  }
 
-    public MatchesResponse(String message) {
-        super("error", message);
-    }
+  public MatchesResponse(String message) {
+    super("error", message);
+  }
 
-    public List<Match> getMatches() {
-        return matches;
-    }
+  public List<Match> getMatches() {
+    return matches;
+  }
 }
