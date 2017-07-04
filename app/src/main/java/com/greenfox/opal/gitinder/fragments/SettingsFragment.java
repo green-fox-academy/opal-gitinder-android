caa50a14da3321f7dfc27a6129143c55c5c6bd8f
balintvecsey <balint.vecsey@gmail.com>
2017-07-04 17:14:29 +0200

    add switches in SettingsFragment

--- app/src/main/java/com/greenfox/opal/gitinder/fragments/SettingsFragment.java
+++ app/src/main/java/com/greenfox/opal/gitinder/fragments/SettingsFragment.java
@@ -7,16 +7,24 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import com.greenfox.opal.gitinder.R;
 
 public class SettingsFragment extends Fragment {
 
   private static final String TAG = "SettingsFragment";
 
+  Switch switchNotifications;
+  Switch switchSync;
+
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     Log.d(TAG, "on Setting tab");
-    return inflater.inflate(R.layout.fragment_settings, container, false);
+    View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+    switchNotifications = (Switch) view.findViewById(R.id.switch_notifications);
+    switchSync = (Switch) view.findViewById(R.id.switch_sync);
+    return view;
   }
 }
