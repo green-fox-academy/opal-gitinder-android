e750c09568a227dfe3edb1656b028012e2d3a249
Nagy Dora <dorinagy06@gmail.com>
2017-07-05 12:47:34 +0200

    display latest message, avatar in match item

--- app/src/main/java/com/greenfox/opal/gitinder/service/MatchesAdapter.java
+++ app/src/main/java/com/greenfox/opal/gitinder/service/MatchesAdapter.java
@@ -1,8 +1,40 @@
 package com.greenfox.opal.gitinder.service;
 
-/**
- * Created by Nagy Dóra on 2017.07.05..
- */
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class MatchesAdapter {
+import com.greenfox.opal.gitinder.R;
+import com.greenfox.opal.gitinder.model.response.Match;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MatchesAdapter extends ArrayAdapter<Match> {
+
+  public MatchesAdapter(@NonNull Context context, ArrayList<Match> matches) {
+    super(context, 0, matches);
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    Match current = getItem(position);
+
+    TextView username = (TextView) convertView.findViewById(R.id.textName);
+    username.setText(current.getUsername());
+
+    TextView latestMessage = (TextView) convertView.findViewById(R.id.textMessage);
+    latestMessage.setText(current.getLatestMessage());
+
+    CircleImageView avatar = (CircleImageView) convertView.findViewById(R.id.imageView);
+    int id = getContext().getResources()
+      .getIdentifier(current.getAvatarUrl(), "drawable", getContext().getPackageName());
+    avatar.setImageResource(id);
+
+    return convertView;
+  }
 }
