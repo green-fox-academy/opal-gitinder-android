573b05fd3fe3db111132c690696c7906e6b0e1f2
balintvecsey <balint.vecsey@gmail.com>
2017-07-10 12:34:17 +0200

    add testcase for Notification switch

--- app/src/test/java/com/greenfox/opal/gitinder/SettingsFragmentTest.java
+++ app/src/test/java/com/greenfox/opal/gitinder/SettingsFragmentTest.java
@@ -1,23 +1,39 @@
 package com.greenfox.opal.gitinder;
 
+import static junit.framework.Assert.assertTrue;
 import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.Switch;
 import com.greenfox.opal.gitinder.fragments.SettingsFragment;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class SettingsFragmentTest {
 
-  private SettingsFragment settingsFragment;
+  SettingsFragment settingsFragment;
+  SharedPreferences sharedPreferences;
 
   @Before
   public void setup() throws Exception {
     settingsFragment = new SettingsFragment();
+    sharedPreferences = RuntimeEnvironment.application.getSharedPreferences("Switch State",
+        Context.MODE_PRIVATE);
     startFragment(settingsFragment);
   }
 
+  @Test
+  public void saveNotificationSwitchStateInSharedPreferences() {
+    Switch switchNotification = (Switch) settingsFragment.getView().findViewById(R.id.switch_notifications);
+
+    switchNotification.performClick();
+    assertTrue(sharedPreferences.contains("Enable Notifications"));
+  }
 }
