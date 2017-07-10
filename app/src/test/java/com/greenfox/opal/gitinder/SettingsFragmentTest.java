d7844facfaa1c23b7a6a7a112684e92e1d90d3e5
balintvecsey <balint.vecsey@gmail.com>
2017-07-10 11:29:34 +0200

    setup test class

--- app/src/test/java/com/greenfox/opal/gitinder/SettingsFragmentTest.java
+++ app/src/test/java/com/greenfox/opal/gitinder/SettingsFragmentTest.java
@@ -1,5 +1,9 @@
 package com.greenfox.opal.gitinder;
 
+import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
+
+import com.greenfox.opal.gitinder.fragments.SettingsFragment;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
@@ -7,5 +11,13 @@ import org.robolectric.annotation.Config;
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class SettingsFragmentTest {
+
+  private SettingsFragment settingsFragment;
+
+  @Before
+  public void setup() throws Exception {
+    settingsFragment = new SettingsFragment();
+    startFragment(settingsFragment);
+  }
 
 }
