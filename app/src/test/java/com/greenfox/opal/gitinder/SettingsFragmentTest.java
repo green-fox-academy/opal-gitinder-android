e53e972fcb167216c5a0a014012de0aae5036c2c
balintvecsey <balint.vecsey@gmail.com>
2017-07-10 10:56:23 +0200

    add SettingsFragmnetTest class

---
+++ app/src/test/java/com/greenfox/opal/gitinder/SettingsFragmentTest.java
@@ -0,0 +1,11 @@
+package com.greenfox.opal.gitinder;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class SettingsFragmentTest {
+
+}
