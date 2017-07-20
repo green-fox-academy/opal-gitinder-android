f157aa32d861ecf6dd06946bd5f0e159e3d5f491
Nagy Dora <dorinagy06@gmail.com>
2017-07-20 08:56:00 +0200

    message activity

--- app/src/main/java/com/greenfox/opal/gitinder/MessageActivity.java
+++ app/src/main/java/com/greenfox/opal/gitinder/MessageActivity.java
@@ -1,5 +1,13 @@
 package com.greenfox.opal.gitinder;
 
-public class MessageActivity {
-  
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MessageActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_messages);
+  }
 }
